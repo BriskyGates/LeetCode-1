4952ae593ea9444b019ac1070262e4875729f554
DinnerHowe <xu001314@163.com>
2017-02-27 18:50:23 +0800

    2

--- 2_Add_Two_Numbers.py
+++ 2_Add_Two_Numbers.py
@@ -6,6 +6,8 @@ Copyright (c) 2017 Xu Zhihao (Howe).  All rights reserved.
 This program is free software; you can redistribute it and/or modify
 
 """
+carray = False
+
 # Definition for singly-linked list.
 class ListNode(object):
     def __init__(self):
@@ -22,13 +24,31 @@ class ListNode_handle:
         node.val = data
         node.next = self.cur_node
         self.cur_node = node
+        return node
 
-    def print_ListNode(self):
-        node = self.cur_node
+    def print_ListNode(self, node):
         while node:
-            print '\n\nnode: ', node, ' value: ', node.val, ' next: ', node.next
+            print '\nnode: ', node, ' value: ', node.val, ' next: ', node.next
             node = node.next
 
+
+    def _reverse(self, nodelist):
+        list = []
+        while nodelist:
+            list.append(nodelist.val)
+            nodelist = nodelist.next
+        result = ListNode()
+        result_handle = ListNode_handle()
+        for i in list:
+            result = result_handle.add(i)
+        return result
+
+    def get_list(self, nodelist):
+        list = []
+        while nodelist:
+            list.append(nodelist.val)
+            nodelist = nodelist.next
+        return list
 
 
 class Solution(object):
@@ -38,23 +58,70 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+        global carry
         carry = False
-        new_node = ListNode()
-
-
-
+        result_handle = ListNode_handle()
+        result = ListNode()
+        while l1 != None or l2 != None:
+            if not carry:
+                if l1 != None and l2 != None:
+                    l3_val = l1.val + l2.val
+                elif l1 == None and l2 != None:
+                    l3_val = l2.val
+                elif l1 != None and l2 == None:
+                    l3_val = l1.val
+                else:
+                    pass
+            else:
+                if l1 != None and l2 != None:
+                    l3_val = l1.val + l2.val + 1
+                elif l1 == None and l2 != None:
+                    l3_val = l2.val + 1
+                elif l1 != None and l2 == None:
+                    l3_val = l1.val + 1
+                else :
+                    pass
+                carry = False
+            if l3_val < 10:
+                result = result_handle.add(l3_val)
+            else:
+                result = result_handle.add(l3_val%10)
+                carry = True
+            try:
+                l1 = l1.next
+            except:
+                pass
+            try:
+                l2 = l2.next
+            except:
+                pass
+        result = result_handle._reverse(result)
+        result_list = []
+        result_list = result_handle.get_list(result)
+        if carry:
+            result_list.append(1)
+            result = result_handle.add(1)
+            result = result_handle._reverse(result)
+        result_handle.print_ListNode(result)
+        return result_list
 
 if __name__ == "__main__":
     # creat 2 linked lists
     ListNode_1 = ListNode_handle()
-    ListNode_1.add(2)
-    ListNode_1.add(4)
-    ListNode_1.add(3)
+    l1 = ListNode()
+    l1_list = [1,8]
+    for i in l1_list:
+        l1 = ListNode_1.add(i)
+
     ListNode_2 = ListNode_handle()
-    ListNode_2.add(5)
-    ListNode_2.add(6)
-    ListNode_2.add(4)
+    l2 = ListNode()
+    l2_list = [9]
+    for i in l2_list:
+        l2 = ListNode_2.add(i)
+    #reverse 2 linked lists
+    l1 = ListNode_1._reverse(l1)
+    l2 = ListNode_2._reverse(l2)
 
-    ListNode_1.print_ListNode()
-    ListNode_2.print_ListNode()
-
+    #get result
+    result = Solution().addTwoNumbers(l1, l2)
+    print result
